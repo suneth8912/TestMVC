cafea828740f4aa03dd6b2c8b4d14a354a6e7f7f
suneth8912 <sunethko@gmail.com>
2017-02-27 20:36:49 +0530

    files addded

---
+++ MVC/Authorize.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC
+{
+    public class Authorize
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public  static bool isAuthorized()
+        {
+            return true;
+        }
+    }
+}
---
+++ MVC/Scripts/Custom/summary.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+
+    $("#gpssearchbtn").on("click", function () {
+        loadPeriod();
+    });
+
+});
+
+function loadPeriod() {
+    var FromPeriod = $('#fromPeriod').val();
+    var ToPeriod = $('#toPeriod').val();
+
+    var loadPeriod = 'GetPeriodList';
+    //if ($("#GPSSummery").validate()) {
+    $.ajax({
+        url: loadPeriod,
+        data: {
+            FromPeriod: FromPeriod,
+            ToPeriod: ToPeriod,
+        },
+        type: "GET",
+        success: function (result) {
+            debugger;
+            var list = [];
+            if (result.length > 0) {
+                for (var i = 0; i < result.length; i++) {
+                    var input = $(result[i].Text)
+                    list.push("<li>" + input.selector + "</li>")
+                }
+                $("#periodList").html(list)
+            }
+        },
+        error: function () {
+            alert("error occured");
+        }
+
+    });
+    //}
+}
